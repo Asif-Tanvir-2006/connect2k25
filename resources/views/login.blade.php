7e2af385cd189b6c77a427c7772793633a7eade6
asif <asiftanvir2006@gmail.com>
2025-05-14 13:29:41 +0530

    init

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="view">
+        <div class="padder">
+
+        </div>
+        <div class="box">
+            <div class="wrapper">
+                <div class="caption">Username</div>
+                <div class="upper">
+                    <input type="text" name="" id="">
+                </div>
+                <div class="caption">Password</div>
+                <div class="lower">
+                    <input type="text" name="" id="">
+                </div>
+                <button>Sign In</button>
+                <div class="cap">Not Registered Yet? <a href=""> Sign Up</a></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
